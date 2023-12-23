40c4eba1c6117abd29899004ca767d8fcdf1b0a6
setalxswe <setalxswe@gmail.com>
2023-12-23 03:37:54 -0800

    city

--- models/city.py
+++ models/city.py
@@ -2,6 +2,7 @@
 """ City Module for HBNB project """
 
 from sqlalchemy.ext.declarative import declarative_base
+
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String
 from sqlalchemy import ForeignKey
@@ -11,6 +12,7 @@ from models.place import Place
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
+
     __tablename__ = "cities"
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
